5a00caab5538e28f208918dca08551289e5a282e
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2023-09-08 20:36:33 +0300

    Task 1+2

---
+++ LW1/Task2.py
@@ -0,0 +1,19 @@
+import cv2
+
+# расширения изображений
+img1 =  cv2.imread('pic2.jpg',cv2.IMREAD_COLOR)
+img2 =  cv2.imread('pic3.bmp',cv2.IMREAD_COLOR)
+
+# флаги для создания окна
+cv2.namedWindow('Display window', cv2.WINDOW_NORMAL)
+#cv2.namedWindow('Display window', cv2.WINDOW_AUTOSIZE)
+#cv2.namedWindow('Display window', cv2.WINDOW_FULLSCREEN)
+
+# флаги для чтения изображения
+img3 = cv2.imread('pic1.png',cv2.IMREAD_COLOR) # цветное изображение
+img4 = cv2.imread('pic1.png',cv2.IMREAD_GRAYSCALE) # серое изображение
+img5 = cv2.imread('pic1.png',cv2.IMREAD_UNCHANGED) # без изменений
+
+cv2.imshow('Display window', img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
