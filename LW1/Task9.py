73c5c7125e05615bd72c61e2f7ce9a50a5831bf9
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2023-09-11 12:45:20 +0300

    Task 9 well done Ура лаба закончилась...

---
+++ LW1/Task9.py
@@ -0,0 +1,19 @@
+import cv2
+
+#  объект VideoCapture для подключения к IP-камере
+cap = cv2.VideoCapture("http://192.168.0.12:8080/video")
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow("Phone's camera", frame)
+        # Ждем нажатия клавиши q для выхода из цикла
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    else:
+        print("Ошибка чтения видео")
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
