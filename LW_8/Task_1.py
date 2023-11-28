4d3d882adbd7116293fb8239d01883423b50f6e5
pluxurypinkphloydrockstar <thedianab15@gmail.com>
2023-11-28 13:41:20 +0300

    обнаружение лиц на видео с вебки с помощью каскадов Хаара

---
+++ LW_8/Task_1.py
@@ -0,0 +1,43 @@
+import cv2
+
+# запуск видеопотока с камеры
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
+
+# загрузка каскада Хаара для обнаружения лиц
+face_cascade = cv2.CascadeClassifier('src/haarcascade_frontalface_default.xml')
+
+# задание кодека и создание объекта VideoWriter для записи видео в файл
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('result_videos/output_1.avi', fourcc, 20.0, (640, 480))
+
+# чтение видеофайла кадр за кадром
+while True:
+    # чтение кадра из видеопотока
+    ret, frame = cap.read()
+
+    if ret:
+        # преобразование кадра в оттенки серого
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # обнаружение лиц на кадре
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        # отрисовка прямоугольников вокруг лиц
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # запись кадра в выходной файл
+        out.write(frame)
+
+        # отображение кадра с обнаруженными лицами
+        cv2.imshow('Video', frame)
+
+        # выход при нажатии клавиши 'esc'
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+# освобождение ресурсов
+cap.release()
+out.release()
+cv2.destroyAllWindows()
